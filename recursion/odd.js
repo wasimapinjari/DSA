8b71cf58196d58caa20f9e2b26c76faffe2951ba
Wasim A Pinjari <axmidx101@gmail.com>
2022-12-21 17:52:05 +0530

    Create odd.js

---
+++ recursion/odd.js
@@ -0,0 +1,18 @@
+// display odd numbers in an array using recursion
+
+function countodd(array) {
+    let result = [];
+    function count(input) {
+        if(input.length === 0) {
+            return;
+        }
+        if(input[0] % 2 !== 0) {
+            result.push(input[0]);
+        }
+        count(input.slice(1));
+    }
+    count(array);
+    return result;
+}
+
+countodd([1,2,3,4,5,6,7,8,9]) // output: [1, 3, 5, 7, 9]
