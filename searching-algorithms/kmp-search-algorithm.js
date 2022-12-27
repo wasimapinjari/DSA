e9120d44197f7592f893b29f66f67bd6b1870681
Wasim A Pinjari <axmidx101@gmail.com>
2022-12-27 11:31:27 +0530

    Update kmp-search-algorithm.js

--- searching-algorithms/kmp-search-algorithm.js
+++ searching-algorithms/kmp-search-algorithm.js
@@ -1,23 +1,20 @@
-// return index of first occurrence of given string in a string
+// return the index of first occurrence of given string in a string
 
 function kmp(haystack, neddle) {
     let lps = [];
     lps[0] = 0;
     let prevPointer = 0;
     let i = 1;
-    let count = 0;
     while (i < neddle.length) {
         if(neddle[prevPointer] === neddle[i]) {
-            lps[i] = prevPointer + 1;
-            prevPointer ++;
-            i += 1;
+            lps[i] = prevPointer++;
+            prevPointer++;
+            i++;
         }
         else if(prevPointer === 0) {
             lps[i] = 0;
-            i += 1;
-        } else {
-            prevPointer = lps[prevPointer - 1];
-        }
+            i++;
+        } else prevPointer = lps[prevPointer - 1];
     }
     
     let m = 0;
@@ -27,17 +24,12 @@ function kmp(haystack, neddle) {
             m++;
             n++;
         } else {
-            if(n === 0) {
-                m++;
-            } else {
-                n = lps[n - 1];
-            }
+            if(n === 0) m++;
+            else n = lps[n - 1];
         }
-        if(n === neddle.length) {
-            return m - neddle.length;
-        }
+        if(n === neddle.length) return m - neddle.length;
     }
     return -1
 }
 
-kmp("xlyliioilyli", "ily") // output: 7
+kmp("heyilysomuch", "ily") // output: 3
